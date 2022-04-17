5cae13d2bf18d9b2b9d494f0f1b59e9b7d437a7e
Shivanshu <shivanshukhanna06@gmail.com>
2022-04-17 23:27:14 +0530

    first

---
+++ src/utils/footerInfo.js
@@ -0,0 +1,13 @@
+export const contactInfo = [
+  { id: 1, text: 'PHONE: Toll Free (+91) 8707394463' },
+  { id: 2, text: 'EMAIL: company@ecommerce.com' },
+  { id: 4, text: 'ADDRESS: 123 Street Name, Ghaziabad, India' },
+  { id: 3, text: 'Mon - Sun / 9:00 AM - 8:00 PM ' },
+];
+
+export const myAccount = [
+  { id: 1, text: 'About Us' },
+  { id: 2, text: 'Returns' },
+  { id: 4, text: 'Custom Service' },
+  { id: 3, text: 'Terms & Condition' },
+];
